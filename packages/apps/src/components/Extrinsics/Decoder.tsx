78ba50b67dca9e24d7de18be65819f2409c62877
cuteolaf <oliverlim818@gmail.com>
2023-06-22 00:39:07 -0700

    fix decoder

--- packages/apps/src/components/Extrinsics/Decoder.tsx
+++ packages/apps/src/components/Extrinsics/Decoder.tsx
@@ -1,7 +1,6 @@
 // Copyright 2017-2022 @polkadot/app-extrinsics authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-// eslint-disable-next-line header/header
 import type { SubmittableExtrinsic, SubmittableExtrinsicFunction } from '@polkadot/api/types';
 import type { Call } from '@polkadot/types/interfaces';
 import type { HexString } from '@polkadot/util/types';
@@ -18,107 +17,110 @@ import { Call as CallDisplay } from '@polkadot/react-params';
 import Decoded from './Decoded.js';
 
 interface Props {
-  className?: string;
-  defaultValue?: HexString | null;
-  setLast: (value: DecodedExtrinsic | null) => void;
+    className?: string;
+    defaultValue?: HexString | null;
+    setLast: (value: DecodedExtrinsic | null) => void;
 }
 
 interface ExtrinsicInfo {
-  decoded: SubmittableExtrinsic<'promise'> | null;
-  extrinsicCall: Call | null;
-  extrinsicError: string | null;
-  extrinsicFn: SubmittableExtrinsicFunction<'promise'> | null;
-  extrinsicHex: string | null;
-  extrinsicKey: string;
-  isCall: boolean;
+    decoded: SubmittableExtrinsic<'promise'> | null;
+    extrinsicCall: Call | null;
+    extrinsicError: string | null;
+    extrinsicFn: SubmittableExtrinsicFunction<'promise'> | null;
+    extrinsicHex: string | null;
+    extrinsicKey: string;
+    isCall: boolean;
 }
 
 const DEFAULT_INFO: ExtrinsicInfo = {
-  decoded: null,
-  extrinsicCall: null,
-  extrinsicError: null,
-  extrinsicFn: null,
-  extrinsicHex: null,
-  extrinsicKey: 'none',
-  isCall: true
+    decoded: null,
+    extrinsicCall: null,
+    extrinsicError: null,
+    extrinsicFn: null,
+    extrinsicHex: null,
+    extrinsicKey: 'none',
+    isCall: true
 };
 
 function Decoder({ className, defaultValue, setLast }: Props): React.ReactElement<Props> {
-  const { encoded } = useParams<{ encoded: string }>();
-  const [initialValue] = useState(() => defaultValue || encoded);
-  const { api } = useApi();
+    const { encoded } = useParams<{ encoded: string }>();
+    const [initialValue] = useState(() => defaultValue || encoded);
+    const { api } = useApi();
 
-  const [{ decoded, extrinsicCall, extrinsicError, extrinsicFn, extrinsicKey, isCall }, setExtrinsicInfo] = useState<ExtrinsicInfo>(DEFAULT_INFO);
+    const [{ decoded, extrinsicCall, extrinsicError, extrinsicFn, extrinsicKey, isCall }, setExtrinsicInfo] = useState<ExtrinsicInfo>(DEFAULT_INFO);
 
-  const _setExtrinsicHex = useCallback(
-    (hex: string): void => {
-      try {
-        assert(isHex(hex), 'Expected a hex-encoded call');
+    const _setExtrinsicHex = useCallback(
+        (hex: string): void => {
+            try {
+                assert(isHex(hex), 'Expected a hex-encoded call');
 
-        let extrinsicCall: Call;
-        let decoded: SubmittableExtrinsic<'promise'> | null = null;
-        let isCall = true;
+                let extrinsicCall: Call;
+                let decoded: SubmittableExtrinsic<'promise'> | null = null;
+                let isCall = true;
 
-        try {
-          // cater for an extrinsic input...
-          decoded = api.tx(hex);
-          extrinsicCall = api.createType('Call', decoded.method);
-          isCall = false;
-        } catch (e) {
-          extrinsicCall = api.createType('Call', hex);
-        }
+                try {
+                    // cater for an extrinsic input...
+                    decoded = api.tx(hex);
+                    extrinsicCall = api.createType('Call', decoded.method);
+                    isCall = false;
+                } catch (e) {
+                    extrinsicCall = api.createType('Call', hex);
+                }
 
-        const { method, section } = api.registry.findMetaCall(extrinsicCall.callIndex);
-        const extrinsicFn = api.tx[section][method];
-        const extrinsicKey = extrinsicCall.callIndex.toString();
+                const { method, section } = api.registry.findMetaCall(extrinsicCall.callIndex);
+                const extrinsicFn = api.tx[section][method];
 
-        if (!decoded) {
-          decoded = extrinsicFn(...extrinsicCall.args);
-        }
+                const extrinsicKey = extrinsicCall.callIndex.toString();
 
-        setExtrinsicInfo({ ...DEFAULT_INFO, decoded, extrinsicCall, extrinsicFn, extrinsicHex: hex, extrinsicKey, isCall });
-        setLast({ call: extrinsicCall, fn: extrinsicFn, hex });
-      } catch (e) {
-        setExtrinsicInfo({ ...DEFAULT_INFO, extrinsicError: (e as Error).message });
-        setLast(null);
-      }
-    },
-    [api, setLast]
-  );
+                if (!decoded) {
+                    decoded = extrinsicFn(...extrinsicCall.args);
+                }
 
-  return (
-    <div className={className}>
-      <Input
-        defaultValue={initialValue}
-        isError={!extrinsicFn}
-        label='hex-encoded call'
-        onChange={_setExtrinsicHex}
-        placeholder='0x...'
-      />
-      {extrinsicError && (
-        <MarkError content={extrinsicError} />
-      )}
-      {extrinsicFn && extrinsicCall && (
-        <>
-          <InputExtrinsic
-            defaultValue={extrinsicFn}
-            isDisabled
-            key={`extrinsicKey:${extrinsicKey}`}
-            label='decoded call'
-          />
-          <CallDisplay
-            className='details'
-            value={extrinsicCall}
-          />
-        </>
-      )}
-      <Decoded
-        extrinsic={decoded}
-        isCall={isCall}
-        withData={false}
-      />
-    </div>
-  );
+                setExtrinsicInfo({ ...DEFAULT_INFO, decoded, extrinsicCall, extrinsicFn, extrinsicHex: hex, extrinsicKey, isCall });
+
+                if (defaultValue !== hex)
+                    setLast({ call: extrinsicCall, fn: extrinsicFn, hex });
+            } catch (e) {
+                setExtrinsicInfo({ ...DEFAULT_INFO, extrinsicError: (e as Error).message });
+                setLast(null);
+            }
+        },
+        [api, setLast]
+    );
+
+    return (
+        <div className={className}>
+            <Input
+                defaultValue={initialValue}
+                isError={!extrinsicFn}
+                label='hex-encoded call'
+                onChange={_setExtrinsicHex}
+                placeholder='0x...'
+            />
+            {extrinsicError && (
+                <MarkError content={extrinsicError} />
+            )}
+            {extrinsicFn && extrinsicCall && (
+                <>
+                    <InputExtrinsic
+                        defaultValue={extrinsicFn}
+                        isDisabled
+                        key={`extrinsicKey:${extrinsicKey}`}
+                        label='decoded call'
+                    />
+                    <CallDisplay
+                        className='details'
+                        value={extrinsicCall}
+                    />
+                </>
+            )}
+            <Decoded
+                extrinsic={decoded}
+                isCall={isCall}
+                withData={false}
+            />
+        </div>
+    );
 }
 
 export default React.memo(Decoder);
